12eeadfcf9e8dc30e51be76b43217d9df08d3999
HappyRadouce <imbabazihappyradoucee51@gmail.com>
2026-03-06 00:30:53 +0200

    digning the login  panel

--- src/ui/LoginForm.java
+++ src/ui/LoginForm.java
@@ -5,4 +5,7 @@ import javax.swing.*;
 public class LoginForm {
     private JTextField textField1;
     private JPasswordField passwordField1;
+    private JCheckBox rememberMeCheckBox;
+    private JButton resetFieldButton;
+    private JButton signInSecurilyButton1;
 }

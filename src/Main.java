01c7ab757355e7be556fbc2eefd2de0f08b0fb5f
HappyRadouce <imbabazihappyradoucee51@gmail.com>
2026-03-20 12:59:41 +0200

    Fix sqllite dependency configurations

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
-public class Main {
+import com.formdev.flatlaf.FlatDarkLaf;
+import ui.LoginForm;
+import javax.swing.*;
 
+public class Main {
     public static void main(String[] args) {
-        ui.LoginForm.main(args);
+        try {
+            UIManager.setLookAndFeel(new FlatDarkLaf());
+        } catch (Exception ex) {
+            System.err.println("Failed to initialize modern theme.");
+        }
+
+        SwingUtilities.invokeLater(() -> {
+            new LoginForm().setVisible(true);
+        });
     }
 }
